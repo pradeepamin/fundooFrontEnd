b7c53d7b4a73fcb5cfa8785a4f98377d47e64410
Pradeep B Amin <pradeepbamin5@gmail.com>
2019-12-30 19:34:00 +0530

    [Pradeep] Refactor modified in dashboard ts added profile pic and its card  and pushed into remote

--- fundooClient/src/app/dashboard/dashboard.component.ts
+++ fundooClient/src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserServiceService } from '../services/user-service.service';
+import { Router } from '@angular/router';
+import { log } from 'util';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  localstorage_image:any
+  imageurl:string
+  fname;
+  lname;
+  email;
+  username;
+  profilePicUser;
+  url;
+  constructor(private userService:UserServiceService,private router:Router) { }
 
   ngOnInit() {
+    this.fname=localStorage.getItem('firstNameUser');
+    this.email=localStorage.getItem('emailUser');
+    this.profilePicUser=localStorage.getItem('profilePicUser');
+    
+    
 
   }
-  getAccountNo(){
-     localStorage.getItem('profilePic');
+  logout()
+{
+  this.userService.logoutUser();
+  localStorage.clear();
+  console.log('User logout')
+  this.router.navigate(['/login']);
 }
+
+
+// onSelectFile(event) {
+//   if (event.target.files && event.target.files[0]) {
+//     var reader = new FileReader();
+
+//     reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+//     reader.onload = (event) => { // called once readAsDataURL is completed
+//       this.url = event.target.result;
+//       console.log("LOGG-->",this.url);
+      
+//     }                                                                                                                                                       
+//   }
+// }
+
+
 
 }
