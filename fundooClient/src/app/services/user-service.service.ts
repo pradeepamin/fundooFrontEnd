d837d7b4342f8f4d66af009b3c0eb4dae9ff3b3d
Pradeep B Amin <pradeepbamin5@gmail.com>
2020-01-09 19:20:51 +0530

    [Pradeep]Refactor. made some changes and pushed into remote

--- fundooClient/src/app/services/user-service.service.ts
+++ fundooClient/src/app/services/user-service.service.ts
@@ -9,7 +9,7 @@ import { log } from 'util';
   providedIn: 'root'
 })
 export class UserServiceService {
-  
+
   baseUrl = environment.baseUrl
 
 
@@ -21,8 +21,8 @@ export class UserServiceService {
 
   register(newUsers) {
     console.log(newUsers, "here in frontend---");
-    console.log("this url",this.baseUrl);
-    console.log("user--->",newUsers);
+    console.log("this url", this.baseUrl);
+    console.log("user--->", newUsers);
     return this.http.post(this.baseUrl + 'register', newUsers)
   }
 
@@ -30,27 +30,30 @@ export class UserServiceService {
     return this.http.post(this.baseUrl + 'forgotPassword', Email)
   }
 
-  resetPassword(passwords,tok){
+  resetPassword(passwords, tok) {
     return this.http.post(this.baseUrl + `resetPassword/${tok}`, passwords)
   }
-  logoutUser(){
-    return this.http.post(this.baseUrl + 'login',{} )
+  logoutUser() {
+    return this.http.post(this.baseUrl + 'login', {})
   }
 
-  uploadProfilePic(file,image){
-    console.log("File and imagee-->",file,image);
+  uploadProfilePic(uploadData) {
 
-    let formdata: FormData = new FormData();
-    formdata.append('image',image);
-
-    const formData = new FormData();
-
-  console.log("Form dattta--->",formData);
-  
-
-    return this.http.post(this.baseUrl + 'imageUpload', formData)
-  }
-  
-  
-
+const HttpUploadOptions = {
+  headers: new HttpHeaders({ "Content-Type": "multipart/form-data" })
 }
+
+    for (var pair of uploadData.entries()) {
+      console.log(pair[0]+ ', ' + pair[1]); 
+  }
+    console.log("Form data-->", uploadData, +"urkl   " + 'http://localhost:4000/user/imageUpload');
+    console.log('http://localhost:4000/user/imageUpload');
+    
+
+    return this.http.post('http://localhost:4000/user/imageUpload', uploadData)
+
+  }
+
+
+
+} 
